4b8bcb208da78a87901da00f34c2e336392de26b
Eva Garcia <emgg2222@gmail.com>
2023-02-06 16:35:55 +0100

    add router

---
+++ src/pages/Desk.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Desk = () => {
+  return (
+    <div>Desk page</div>
+  )
+}
---
+++ src/pages/RouterPage.js
@@ -0,0 +1,78 @@
+import React  from 'react';
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu} from 'antd';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Link  
+    
+  } from "react-router-dom";
+import { GetInto } from './GetInto';
+import { Queue } from './Queue';
+import { CreateTicket } from './CreateTicket';
+import { Desk } from './Desk';
+
+const { Sider, Content } = Layout;
+const colorBgContainer = 'rgba(255, 255, 255, 0.2)';
+
+export const RouterPage = () => {
+  return (
+    <Router>
+    <Layout style={{ height: '100vh' }}>
+        <Sider>
+        <div className="logo" />
+        <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+            {
+                key: '1',
+                icon: <UserOutlined />,
+                label: <Link to='/getInto'>Get into</Link>,
+                //link: <Link to="/getInto" >Get Into</Link>
+            },
+            {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: <Link to='/queue'>Queue</Link>,
+            },
+            {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: <Link to='/newTicket'>Create Ticket </Link>,
+            },
+            ]}
+        />
+        </Sider>
+        <Layout className="site-layout">
+   
+        <Content
+            style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            }}
+        >
+
+                <Routes>
+                    <Route path='/getInto' element={ <GetInto /> } />
+                    <Route path='/queue' element={ <Queue /> } />
+                    <Route path='/newTicket' element={ <CreateTicket /> } />
+                    <Route path='/desk' element={ <Desk /> } />
+                    <Route path="*" element={ <GetInto /> } />
+            </Routes>
+
+            
+        </Content>
+        </Layout>
+    </Layout>    
+  </Router>
+  )
+}
